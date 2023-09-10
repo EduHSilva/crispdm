22cb1b209afb9279759238cf86732d148e82c9c3
Eduardo <edu.slhenrique@gmail.com>
2023-09-09 21:09:16 -0300

    atualizacao

--- README.md
+++ README.md
@@ -1,13 +1,78 @@
-# Problema abordado
-Investidores do mercado imobiliário buscam identificar oportunidades de investimento, realizar comparações entre diversas 
-propriedades e antecipar possíveis retornos. Nesse contexto, é fundamental contar com suporte na tomada de decisões estratégicas.
+# Utilizando Análise de Dados para Auxiliar Investidores no Mercado Imobiliário
 
-# Solução proposta
-Nossa solução proposta envolve a aplicação de modelos preditivos, incluindo regressão e análise de séries temporais, 
-para prever o valor futuro de um imóvel e estimar o potencial retorno para o investidor. Além disso, por meio de uma
-intuitiva e interativa dashboard, oferecemos a capacidade de comparar várias propriedades lado a lado, fornecendo uma
-visão abrangente do mercado imobiliário e das opções de investimento disponíveis.
+## Business understanding
+
+### Contexto
+
+O mercado imobiliário é uma arena complexa e dinâmica, onde os investidores buscam oportunidades de investimento sólidas
+e desejam tomar decisões estratégicas informadas. Identificar as propriedades certas, prever seu desempenho futuro e
+calcular os retornos esperados são desafios críticos que os investidores enfrentam. Nesse contexto, a aplicação da
+metodologia CRISP-DM (Cross-Industry Standard Process for Data Mining) desempenha um papel fundamental.
+
+### Problema Abordado:
+
+Investidores no mercado imobiliário enfrentam a necessidade de identificar oportunidades de investimento, comparar
+várias propriedades e antecipar os retornos potenciais. Para tomar decisões estratégicas eficazes, é essencial contar
+com suporte baseado em dados e análises confiáveis.
+
+### Solução Proposta:
+
+A solução proposta envolve a aplicação de modelos preditivos avançados, como regressão e análise de séries temporais,
+para prever o valor futuro de imóveis e estimar os potenciais retornos para os investidores. Além disso, por meio de uma
+dashboard intuitiva e interativa, oferecemos a capacidade de comparar várias propriedades lado a lado. Essa abordagem
+fornece aos investidores uma visão abrangente do mercado imobiliário, permitindo que eles tomem decisões informadas e
+estratégicas em seus investimentos.
+
+### Benefícios para os Investidores:
+
+- Identificação mais precisa de oportunidades de investimento.
+- Estimativas confiáveis de retornos futuros.
+- Comparação fácil de várias propriedades.
+- Melhor compreensão do mercado imobiliário.
+- Apoio decisivo na tomada de decisões estratégicas.
+- Esta solução não apenas aumenta a eficácia das decisões dos investidores, mas também oferece uma vantagem competitiva
+  no mercado imobiliário em constante mudança, tornando o processo de investimento mais transparente e orientado por
+  dados.
+
+### Objetivo
+
+Treinar um modelo para prever o ROI (retorno sobre investimento) de um imóvel e gerar uma dashboard para analise de
+propriedades.
+
+## Data understanding
+
+O dataset foi gerado pelo CHATGPT com as seguintes colunas:
+
+| Coluna                           | Tipo    | Descrição                                                                |
+|----------------------------------|---------|--------------------------------------------------------------------------|
+| id                               | int64   | Identificador único auto-incrementável                                   |
+| Tipo de imóvel                   | object  | Se é casa, apartamento ou condomínio                                     |
+| Localização                      | object  | Cidade do imovel (indo de cidade 1 a 10)                                 |
+| Coordenadas                      | object  | Pontos geograficos do imovel                                             |
+| Número de Quartos                | int64   | Numéro de quartos do imovel                                              |
+| Número de Banheiros              | int64   | Numéro de quartos do imovel                                              |
+| Área Total (em metros quadrados) | float64 | Área do imovel                                                           |
+| Idade da Propriedade             | int64   | Diferença entre ano atual e ano de construção da propriedade             |
+| Condição da Propriedade          | object  | Condição atual do propriedade podendo ser: Excelente, Boa, Média ou Ruim |
+| Amenidades                       | object  | Se o imovel possui garagem, piscina, jardim, vista panoramica            |
+| Preço de Venda Anterior          | int64   | Preço da ultima venda do imovel                                          |
+| Data de Venda Anterior           | date    | Data de ultima venda do imovel                                           |
+| Histórico de aluguel             | bool    | Se o imovel foi alugado                                                  |
+| Valor de aluguel                 | float64 | Valor de aluguel do imóvel                                               |
+| Taxa de juros atuais             | float64 | Valor de juros do imóvel                                                 |
+| Custos de manutenção anuais      | float64 | Média de gastos com manutenção ao longo dos anos                         |
+| Taxas de Condomínio Mensais      | float64 | Taxas atuais a serem pagas ao condominio do imovel                       |
+| Impostos sobre a Propriedade     | float64 | Valor gastos com impostos anualmente                                     |
+| Histórico de Valorização         | float64 | Valor entre 0.1 a 0.5 com taxa de valorização historica                  |
+| Fluxos de Caixa Anuais           | float64 | Possivel retorno em caixa anual                                          |
+| Retorno sobre Investimento (ROI) | float64 | Retorno sobre investimento                                               |
+| Preço de venda atual             | float64 | Valor de venda do imóvel                                                 |
+| Data de Inclusão no Dataset      | date    | Data de inclusão no dataset                                              |
+| Fonte dos Dados                  | object  | Origem de dados                                                          |
+
+As informações sobre a propriedade, como idade, condição, amenidades, histórico de venda e aluguel, bem como dados
+financeiros relevantes, como taxas de juros, custos de manutenção, impostos podem ser relevantes para o objetivo do
+algoritmo.
 
 
-
-
+## Data preparation
--- main.py
+++ main.py
@@ -1,60 +1,117 @@
 import os
-
 import pandas as pd
+import numpy as np
 import random
 import datetime
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 from ydata_profiling import ProfileReport
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
 
 
 def generate_data(path_file: str, num_rows: int) -> None:
     data: pd.DataFrame = pd.DataFrame(columns=[
-        'ID da Propriedade', 'Tipo de Imóvel', 'Localização', 'Coordenadas Geográficas',
+        'ID da Propriedade', 'Tipo de Imóvel', 'Localização',
         'Número de Quartos', 'Número de Banheiros', 'Área Total (em metros quadrados)',
         'Idade da Propriedade', 'Condição da Propriedade', 'Amenidades', 'Preço de Venda Anterior',
-        'Data de Venda Anterior', 'Histórico de Aluguel', 'Características do Bairro',
-        'Tendências de Mercado', 'Taxas de Juros Atuais', 'Custos de Manutenção Anuais',
+        'Data de Venda Anterior', 'Histórico de Aluguel', 'Taxas de Juros Atuais', 'Custos de Manutenção Anuais',
         'Taxas de Condomínio Mensais', 'Impostos sobre a Propriedade', 'Histórico de Valorização',
         'Fluxos de Caixa Anuais', 'Retorno sobre Investimento (ROI)', 'Data de Inclusão no Dataset',
         'Fonte dos Dados'
     ])
 
-    for _ in range(num_rows):
+    idade_imovel = np.random.randint(1, 50, num_rows)
+    preco_anterior = np.random.randint(50000, 5000000, num_rows)
+    area_total = np.random.uniform(50, 300, num_rows)
+    taxas_juros = np.random.uniform(2, 6, num_rows)
+    valor_alugueis = np.random.randint(500, 5000, num_rows)
+    custos_manutencao_anual = np.random.uniform(100, 5000, num_rows)
+    impostos_sobre_imovel = np.random.uniform(500, 5000, num_rows)
+    taxas_condominio = np.random.uniform(50, 500, num_rows)
+
+    roi = (0.2 * idade_imovel + 0.4 * preco_anterior + 0.2 * area_total + 0.3 + taxas_juros * 0.2
+           + 0.4 * valor_alugueis + 0.3 * custos_manutencao_anual + 0.1 * impostos_sobre_imovel + 0.1 * taxas_condominio
+           + np.random.normal(0, 2, num_rows))
+
+    for i in range(num_rows):
+        id_propriedade: int = i
+        print(i)
+
         data = data._append({
-            'ID da Propriedade': random.randint(1, 10000),
+            'ID da Propriedade': id_propriedade,
             'Tipo de Imóvel': random.choice(['Casa', 'Apartamento', 'Condomínio']),
             'Localização': f'Cidade {random.randint(1, 10)}',
-            'Coordenadas Geográficas': f'({random.uniform(-90, 90)}, {random.uniform(-180, 180)})',
             'Número de Quartos': random.randint(1, 6),
             'Número de Banheiros': random.randint(1, 4),
-            'Área Total (em metros quadrados)': random.uniform(50, 300),
-            'Idade da Propriedade': random.randint(1, 50),
+            'Área Total (em metros quadrados)': area_total[i],
+            'Idade da Propriedade': idade_imovel[i],
             'Condição da Propriedade': random.choice(['Excelente', 'Boa', 'Média', 'Ruim']),
             'Amenidades': ', '.join(random.sample(['Piscina', 'Garagem', 'Jardim', 'Vista panorâmica'], 2)),
-            'Preço de Venda Anterior': random.randint(50000, 5000000),
+            'Preço de Venda Anterior': preco_anterior[i],
             'Data de Venda Anterior': datetime.date(random.randint(1980, 2023), random.randint(1, 12),
                                                     random.randint(1, 28)),
             'Histórico de Aluguel': random.choice(['Sim', 'Não']),
-            'Características do Bairro': f'Bairro {random.randint(1, 20)}',
-            'Tendências de Mercado': random.choice(['Crescente', 'Estável', 'Decrescente']),
-            'Taxas de Juros Atuais': random.uniform(2, 6),
-            'Custos de Manutenção Anuais': random.uniform(100, 5000),
-            'Taxas de Condomínio Mensais': random.uniform(50, 500),
-            'Impostos sobre a Propriedade': random.uniform(500, 5000),
+            'Valor aluguel': valor_alugueis[i],
+            'Taxas de Juros Atuais': taxas_juros[i],
+            'Custos de Manutenção Anuais': custos_manutencao_anual[i],
+            'Taxas de Condomínio Mensais': taxas_condominio[i],
+            'Impostos sobre a Propriedade': impostos_sobre_imovel[i],
             'Histórico de Valorização': random.uniform(0.1, 0.5),
             'Fluxos de Caixa Anuais': random.uniform(1000, 50000),
-            'Retorno sobre Investimento (ROI)': random.uniform(0.05, 0.2),
+            'Retorno sobre Investimento (ROI)': roi[i],
+            'Preço de Venda Atual': random.randint(50000, 5000000),
             'Data de Inclusão no Dataset': datetime.date(random.randint(2010, 2023), random.randint(1, 12),
                                                          random.randint(1, 28)),
             'Fonte dos Dados': random.choice(['Zillow', 'Kaggle', 'Redfin'])
         }, ignore_index=True)
 
-    data.to_csv(path_file, index=False)
+        data.to_csv(path_file, index=False)
+
+
+def train_and_evaluate_decision_tree(df):
+    le = LabelEncoder()
+    data_encoded = df.apply(lambda col: le.fit_transform(col) if col.dtype == 'object' else col)
+
+    y = data_encoded['Retorno sobre Investimento (ROI)']
+    x = data_encoded.drop(columns=['Retorno sobre Investimento (ROI)'])
+
+    x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.2, random_state=23)
+
+    decision_tree_model = DecisionTreeRegressor()
+    decision_tree_model.fit(x_treino, y_treino)
+
+    predicoes = decision_tree_model.predict(x_teste)
+
+    score = r2_score(y_teste, predicoes)
+    print("Decision tree R² Score:", score)
+
+
+def train_and_evaluate_linear_regression(df):
+    le = LabelEncoder()
+    data_encoded = df.apply(lambda col: le.fit_transform(col) if col.dtype == 'object' else col)
+
+    y = data_encoded['Retorno sobre Investimento (ROI)']
+    x = data_encoded.loc[:, df.columns != 'Retorno sobre Investimento (ROI)']
+
+    x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.2, random_state=23)
+
+    linear_regression_model = LinearRegression()
+    linear_regression_model.fit(x_treino, y_treino)
+    score = linear_regression_model.score(x_teste, y_teste)
+    print('LinearRegression: ', score)
 
 
 if __name__ == '__main__':
     path: str = './data/dataset_imobiliario.csv'
     if not os.path.isfile(path):
-        generate_data(path, 20000)
+        generate_data(path, 10000)
 
     df: pd.DataFrame = pd.read_csv(path)
     profile = ProfileReport(df, title="Profiling Report")
+    profile.to_file("report.html")
+    train_and_evaluate_decision_tree(df)
+
+    train_and_evaluate_linear_regression(df)
